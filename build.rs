00daea1550e6ee19c4718499bc4cdd1378f7f667
Marc-Antoine Manningham <marc-antoine.m@outlook.com>
2025-06-12 10:52:09 -0400

    chore: update llvm bundler

--- build.rs
+++ build.rs
@@ -123,8 +123,7 @@ fn get_system_libcpp() -> Option<&'static str> {
 
 fn llvm_config(argument: &str) -> Result<String, Box<dyn Error>> {
     let prefix = env::var(format!("TABLEGEN_{}0_PREFIX", LLVM_MAJOR_VERSION))
-        .map(|path| Path::new(&path).join("bin"))
-        .unwrap_or_default();
+        .map(|path| Path::new(&path).join("bin"))?;
 
     let llvm_config_exe = if cfg!(target_os = "windows") {
         "llvm-config.exe"
